1cc57d7d1b8d52011ee7acd9f82fcb7463d4928c
BasileLT <blt_git@pm.me>
2024-10-24 15:40:55 +0200

    fixes

--- TP-test/my_bash_NP.sh
+++ TP-test/my_bash_NP.sh
@@ -3,11 +3,16 @@ set -euxo pipefail
 
 cd /home/vagrant
 
-apt-get update
-apt-get install -y python3 python3-pip python3-venv
+# Install python3, pip3 and python3-venv
+# Can't use python3-venv because of a bug in Ubuntu
+sudo apt-get update
+sudo apt-get install -y python3 python3-pip #python3-venv
 
-python3 -m venv .venv
-source .venv/bin/activate
-pip install ansible --no-input --no-cache-dir
+#python3 -m venv .venv
+#source .venv/bin/activate
+pip3 install ansible --no-input --no-cache-dir
 
-ansible-playbook -i 'localhost,' play-TP-Test-NP.yml
+# Add ansible to path
+export PATH=$PATH:/home/vagrant/.local/bin
+
+ansible-playbook -i 'localhost,' localhost play-TP-Test-NP.yml
